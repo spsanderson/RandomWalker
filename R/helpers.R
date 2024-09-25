2f819730727f51dc3db2506e90854fabb870011f
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2024-09-24 20:56:56 -0400

    Fixes #93

--- R/helpers.R
+++ R/helpers.R
@@ -244,3 +244,72 @@ std_cum_sum_augment <- function(.data,
 
   return(ret)
 }
+
+#' Augment Cumulative Product
+#'
+#' @family Utility Functions
+#' @author Steven P. Sanderson II, MPH
+#' @description This function augments a data frame by adding cumulative product
+#' columns for specified variables.
+#'
+#' @details The function takes a data frame and a column name (or names) and
+#' computes the cumulative product for each specified column, starting from an
+#' initial value. If the column names are not provided, it will throw an error.
+#'
+#' @param .data A data frame to augment.
+#' @param .value A column name or names for which to compute the cumulative product.
+#' @param .names Optional. A character vector of names for the new cumulative
+#' product columns. Defaults to "auto", which generates names based on the original
+#' column names.
+#' @param .initial_value A numeric value to start the cumulative product from.
+#' Defaults to 1.
+#'
+#' @return A tibble with the original data and additional columns containing the
+#' cumulative products.
+#'
+#' @examples
+#' library(dplyr)
+#' df <- tibble(x = 1:5, y = 6:10)
+#' std_cum_prod_augment(df, .value = x)
+#' std_cum_prod_augment(df, .value = y, .names = c("cumprod_y"))
+#'
+#' @name std_cum_prod_augment
+NULL
+#' @rdname std_cum_prod_augment
+#' @export
+
+std_cum_prod_augment <- function(.data,
+                                 .value,
+                                 .names = "auto",
+                                 .initial_value = 1) {
+  column_expr <- rlang::enquo(.value)
+
+  if (rlang::quo_is_missing(column_expr)) {
+    stop(call. = FALSE, "std_cum_prod_augment(.value) is missing.")
+  }
+
+  col_nms <- names(tidyselect::eval_select(column_expr, .data))
+
+  make_call <- function(col) {
+    rlang::expr(!!.initial_value * cumprod(1 + !!rlang::sym(col)))
+  }
+
+  grid <- expand.grid(
+    col = col_nms,
+    stringsAsFactors = FALSE
+  )
+
+  calls <- purrr::pmap(.l = list(grid$col), make_call)
+
+  if (any(.names == "auto")) {
+    newname <- paste0("cum_prod_", grid$col)
+  } else {
+    newname <- as.list(.names)
+  }
+
+  calls <- purrr::set_names(calls, newname)
+
+  ret <- tibble::as_tibble(dplyr::mutate(.data, !!!calls))
+
+  return(ret)
+}
